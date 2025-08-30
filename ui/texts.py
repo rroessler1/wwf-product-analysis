8a63b233f14f30c0288c8bf36dcaeb7bb2cb6f33
Ross Roessler <rossroessler@gmail.com>
2025-08-30 11:24:24 +0200

    Add instructions for how to create a zipfile.

--- ui/texts.py
+++ ui/texts.py
@@ -1,7 +1,17 @@
 # UI
 UI_TITLE = "BBQ GPT"
 UI_SUBTITLE = "This tool processes supermarket leaflets and extracts all products with a categorization if they are grillable or not."
-INSTRUCTIONS = "Please upload a zipfile of PDFs and images."
+INSTRUCTIONS = (
+"""Please upload a zipfile of PDFs and images.
+
+To create a zipfile:
+
+1. Hold down the Command (⌘) key on your keyboard and click each file you want. To select everything in the folder: Press Command (⌘) + A.
+2. Right-click on one of the highlighted files.
+3. From the menu that appears, choose “Compress [number] Items” (oder „[Anzahl] Objekte komprimieren“).
+4. Your Mac will create a new file called Archive.zip in the same folder.
+"""
+)
 
 # Navigation
 NAVIGATION = "Navigation"
